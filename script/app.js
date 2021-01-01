03e3cdb39f1a86ae2c33ea99bb18c6878fc7581c
olisaemekaejiofor <38292210+olisaemekaejiofor@users.noreply.github.com>
2021-01-01 22:39:15 +0100

    First of many more comits

---
+++ script/app.js
@@ -0,0 +1,23 @@
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function () {
+        let mode = this.dataset.mode
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode) {
+    if (mode == 'light') {
+        document.getElementById('theme-style').href = "css/style.css"
+    }
+    if (mode == 'blue') {
+        document.getElementById('theme-style').href = "css/blue.css"
+    }
+    if (mode == 'green') {
+        document.getElementById('theme-style').href = "css/green.css"
+    }
+    if (mode == 'purple') {
+        document.getElementById('theme-style').href = "css/purple.css"
+    }
+}
