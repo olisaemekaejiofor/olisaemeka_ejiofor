c790901d6e5f763344eddc0c0916c9bc5aeff2d0
olisaemekaejiofor <38292210+olisaemekaejiofor@users.noreply.github.com>
2021-01-04 22:38:10 +0100

    made some changes

--- script/contact.php
+++ script/contact.php
@@ -1,3 +1,14 @@
 <?php 
-echo 'hello world';
+    if(!empty($_POST['send'])){
+        $name = $_POST['name'];
+        $what = $_POST['what'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+    $to = 'olisehemeke37@gmail.com';
+    $mailHeaders = "From: " . $name . "<" . $email . ">\r\n";
+    mail($to, $what, $message, $mailHeaders);
+
+    
+    }
 ?>
